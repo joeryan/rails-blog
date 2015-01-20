3aca85de6b369da3d0d2ff0b4c00911321cd9d88
Joe Ryan <joe@clanryan.us>
2015-01-19 22:58:05 -0500

    add create function, links to articles, update .gitignore for emacs backups

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     # TODO new article
   end
 
   def create
-    render plain: params[:article].inspect
-    # TODO create the new article
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
